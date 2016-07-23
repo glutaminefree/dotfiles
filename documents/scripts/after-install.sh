aaa60bf966680e04b952ad72f035cf927b0e86aa
glutaminefree <glutaminefree@mail.ru>
2016-07-23 17:27:56 +0300

    Add OS init srcript

---
+++ documents/scripts/after-install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+###########################################
+#                                         #
+# Script for installing soft/copy configs #
+# on fresh Fedora                         #
+#                                         #
+# Required:                               #
+#                                         #
+# * Yandex.Disk full sync                 #
+###########################################
+
+# soft
+echo 'Updating soft'
+sudo dnf update -y
+
+echo 'Installing soft for work'
+sudo dnf install -y\
+    zsh git git-cola htop wget httpd php\
+    vim vim-X11 ctags-etags\
+    ranger highlight atool lynx mediainfo\
+    keepassx thunderbird gimp viewnior poedit\
+    dropbox vlc
+
+# default web site
+echo 'Creating default web site'
+sudo mkdir /var/www/html/dev.loc/ -p
+sudo chown glutaminefree:glutaminefree /var/www/html/dev.loc/ -R
+mkdir /var/www/html/dev.loc/cgi-bin
+cp ~/Yandex.Disk/backups/linux/configs/httpd/cgi-bin/php5 /var/www/html/dev.loc/cgi-bin/php5
+chmod 755 /var/www/html/dev.loc/ -R
+touch /var/www/html/dev.loc/index.php
+chmod 644 /var/www/html/dev.loc/index.php
+echo '<?php phpinfo();' >> /var/www/html/dev.loc/index.php
+
+# copy configs
+echo 'Copying httpd/selinux configs'
+sudo cp /home/glutaminefree/Yandex.Disk/backups/linux/configs/httpd/conf.d/php.conf /etc/httpd/conf.d/php.conf
+sudo cp /home/glutaminefree/Yandex.Disk/backups/linux/configs/httpd/conf.d/sites.conf /etc/httpd/conf.d/sites.conf
+sudo cp /home/glutaminefree/Yandex.Disk/backups/linux/configs/httpd/conf.modules.d/10-php.conf /etc/httpd/conf.modules.d/10-php.conf
+sudo cp /home/glutaminefree/Yandex.Disk/backups/linux/configs/selinux/config /etc/selinux/config
+sudo chmod 600 /etc/selinux/config
+
+echo 'Done'
+echo 'Need to restart!'
