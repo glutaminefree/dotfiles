c26feb319597588ce0da138fb9f72f7ec4d37a65
glutaminefree <glutaminefree@mail.ru>
2016-08-25 09:05:32 +0300

    added composer install to after-install script

--- documents/scripts/after-install.sh
+++ documents/scripts/after-install.sh
@@ -40,6 +40,15 @@ touch /var/www/html/dev.loc/index.php
 chmod 644 /var/www/html/dev.loc/index.php
 echo '<?php phpinfo();' >> /var/www/html/dev.loc/index.php
 
+# install composer
+cd ~
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo cp /home/glutaminefree/composer.phar /url/local/bin/composer
+sudo chmod 755 /url/local/bin/composer
+
 # copy configs
 echo 'Copying httpd/selinux configs'
 sudo cp /home/glutaminefree/Yandex.Disk/backups/linux/configs/httpd/conf.d/php.conf            /etc/httpd/conf.d/php.conf
