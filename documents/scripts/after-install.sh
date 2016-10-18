3984932cf1b4d8a9a080b2857fc402bc7a450e56
glutaminefree <glutaminefree@mail.ru>
2016-10-18 12:02:04 +0300

    added image viewer to soft list

--- documents/scripts/after-install.sh
+++ documents/scripts/after-install.sh
@@ -24,7 +24,7 @@ sudo dnf install -y \
     vim vim-X11 ctags-etags \
     ranger highlight atool lynx mediainfo \
     filezilla poedit gimp \
-    keepassx thunderbird viewnior \
+    keepassx thunderbird viewnior feh \
     dropbox vlc qbittorrent \
 #vagrant plugin install vagrant-vbguest
 
